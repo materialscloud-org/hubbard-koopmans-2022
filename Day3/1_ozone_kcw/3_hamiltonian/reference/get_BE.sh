2f7994ad1a15793fc49346adf4f4ec29896190fd
ncolonna <nicola.colonna@psi.ch>
2022-10-13 10:44:15 +0200

    Update ex

--- Day3/1_ozone_kcw/3_hamiltonian/reference/get_BE.sh
+++ Day3/1_ozone_kcw/3_hamiltonian/reference/get_BE.sh
@@ -1,4 +1,4 @@
-grep "KI\[Full\]" kc.kho | sed -e "s/KI\[Full\]//g" | xargs -n 1  | awk '{print -NR+10, -$1}' | head -9 > ki.dat
-grep "KS " kc.kho | head -2 | sed -e "s/KS//g" | xargs -n 1  | awk '{print -NR+10, -$1}' | head -9 > dft.dat
-paste dft.dat ki.dat BE_exp.dat | head -10 | awk '{print $1, $2, $4,$6}' > BE.dat
+grep "KI\[Full\]" kc.kho | sed -e "s/KI\[Full\]//g" | xargs -n 1  | awk '{print -$1}' | head -9 > ki.dat
+grep "KS " kc.kho | head -2 | sed -e "s/KS//g" | xargs -n 1  | awk '{print -$1}' | head -9 > dft.dat
+paste dft.dat ki.dat BE_exp.dat | head -10 | awk '{print $1, $2, $3}' > BE.dat
 rm dft.dat ki.dat
