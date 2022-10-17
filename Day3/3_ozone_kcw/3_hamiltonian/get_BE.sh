716641a3f90d4e03fae4f1db87e7a1d695ac5e82
Nicola Colonna <nicola.colonna@psi.ch>
2022-10-17 14:02:44 +0200

    Update BE script

--- Day3/3_ozone_kcw/3_hamiltonian/get_BE.sh
+++ Day3/3_ozone_kcw/3_hamiltonian/get_BE.sh
@@ -1,4 +1,5 @@
-grep "KI\[Full\]" kc.kho | sed -e "s/KI\[Full\]//g" | xargs -n 1  | awk '{print -$1}' | head -9 > ki.dat
+grep "KI\[Full\]" kc.kho | sed -e "s/KI\[Full\]//g" | xargs -n 1  | awk '{print -$1}' | head -9 > kcw.dat
 grep "KS " kc.kho | head -2 | sed -e "s/KS//g" | xargs -n 1  | awk '{print -$1}' | head -9 > dft.dat
-paste dft.dat ki.dat BE_exp.dat | head -10 | awk '{print $1, $2, $3}' > BE.dat
-rm dft.dat ki.dat
+grep -A 2 "  Eigenvalues (eV), kp =   1 , spin =  1" ../../1_ozone_kcp/final/ki_final.cpo | tail -1 | xargs -n 1  | awk '{print -$1}' | head -9 > kcp.dat
+paste dft.dat kcw.dat kcp.dat BE_exp.dat | head -10 | awk '{print $1, $2, $3, $4}' > BE.dat
+rm dft.dat kcw.dat kcp.dat
