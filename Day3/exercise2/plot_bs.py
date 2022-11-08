81c9c57cab5346be6bb948fa498e1b5e7f6d2499
Edward Linscott <edwardlinscott@gmail.com>
2022-11-08 10:33:08 +0100

    Made plot_bs.py executable

--- Day3/exercise2/plot_bs.py
+++ Day3/exercise2/plot_bs.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
 from koopmans.io import read
 
-
+# Load the workflow object from file
 wf = read('si.kwf')
-
 
 # Extract the KI band structure from the last Unfold&Interpolate calculation
 ki_bands = wf.calculations[-1].results['band structure']
@@ -12,9 +10,8 @@ ki_bands = wf.calculations[-1].results['band structure']
 # Extract the LDA band structure from the last PW bands calculation
 pw_bands = [c.results['band structure'] for c in wf.calculations if c.prefix == 'bands'][0]
 
-w90_calcs = [c for c in wf.calculations if c.prefix == 'wann']
-
 # Extract the LDA band structure (valence) interpolated with Wannier90
+w90_calcs = [c for c in wf.calculations if c.prefix == 'wann']
 w90_occ_bands = w90_calcs[-2].results['band structure']
 
 # Extract the LDA band structure (conduction) interpolated with Wannier90
@@ -26,6 +23,7 @@ refpw = pw_bands.reference
 # Energy reference for the KI bands
 refcp = [c.results['homo_energy'] for c in wf.calculations if c.prefix == 'dft_init'][0]
 
+# Plot the band structures
 ax = w90_occ_bands.subtract_reference(refpw).plot(color='red', label='LDA W90 interp')
 ax = w90_emp_bands.subtract_reference(refpw).plot(ax=ax, color='red')
 ax = pw_bands.subtract_reference(refpw).plot(ax=ax, ls='none', marker='o', markersize=3, color='blue', label='LDA explicit')
